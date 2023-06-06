4c644d97b8d9dcb4ae1b167b37e26fac95e539a8
LiaoKong <568250549@qq.com>
2023-06-06 23:58:41 +0800

    开始写进度条组件

---
+++ chroma_wdigets/components/widgets/progress_bar.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from Qt import QtWidgets
+from Qt import QtCore
+from Qt import QtGui
+
+
+class ProgressBar(QtWidgets.QProgressBar):
+    pass
